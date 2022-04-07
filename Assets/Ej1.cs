c92ce9a7d14bb519e5c01a0f884458307c48dc40
MaxxZimm <101809163+MaxxZimm@users.noreply.github.com>
2022-04-07 10:24:57 -0300

    Initial commit

---
+++ Assets/Ej1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej1 : MonoBehaviour
+{
+    public int Numero1;
+    public int Numero2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Debug.Log("Escriba ds numeros para ver si son iguales");
+
+        if(Numero1 == Numero2)
+        {
+            Debug.Log("Son el mismo numero");
+        }
+        else
+        {
+            Debug.Log("Son numeros diferentes");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Ej2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej2 : MonoBehaviour
+{
+    public int Producto1;
+    public int Producto2;
+    public int Producto3;
+    public int Dinero;
+
+    void Start()
+    {
+        int total = Producto1 + Producto2 + Producto3;
+        if (Dinero < Producto1 + Producto2 + Producto3)
+        {
+            Debug.Log("No tienes dinero suficiente, te faltan " + (Dinero - total)+"$");
+        }
+        else if (Dinero > Producto1 + Producto2 + Producto3)
+        {
+            Debug.Log("Te sobran " + (Dinero - total)+"$");
+        }
+        else
+        {
+            Debug.Log("Tenes la cantidad de dinero justa");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
